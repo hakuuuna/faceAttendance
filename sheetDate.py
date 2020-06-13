b39e959f61ec54df5a28e4de166674ddcd2c28ba
nasreddino <35689389+nasreddino@users.noreply.github.com>
2020-06-13 03:49:21 +0000

    update
    update

--- sheetDate.py
+++ sheetDate.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import csv
 import sys
+import re
 import os.path
 from tkinter import messagebox
 from PIL import Image,ImageTk
@@ -10,23 +11,20 @@ import subprocess
 
 
 root = tk.Tk()
-root.title("Swim School")
+root.title("sheet date")
 #Tikinter Vars
-username = tk.StringVar()
-password = tk.StringVar()
+
 name = tk.StringVar()
-loggedInLabel = tk.StringVar()
 hours = tk.IntVar()
 minutes = tk.IntVar()
 #Functions
+
+
+
+
 def setup():
 	#Used to make the two textfiles if they don't already exist
-	file_exists = os.path.isfile("users.txt")
-	if file_exists:
-		pass
-	else:
-		file = open("users.txt", "w+")
-		file.close()
+
 	file_exists = os.path.isfile("dates.txt")
 	if file_exists:
 		pass
@@ -38,37 +36,46 @@ def setup():
 	
 def makeAppointment():
 	#Format date
-	date = str(datePickercalendar.day_selected)+"/"+"0"+str(datePickercalendar.month_selected)+"/"+str(datePickercalendar.year_selected)
+	day = str(datePickercalendar.day_selected)
+	month = str(datePickercalendar.month_selected)
+	
+	
+	
+	if datePickercalendar.day_selected < 10:
+	    day = "0"+str(datePickercalendar.day_selected)
+	    
+	if datePickercalendar.month_selected < 10:
+	    month = "0"+str(datePickercalendar.month_selected)
+	    
+	
+	    
+	    
+	    
+	date = day+"/"+month+"/"+str(datePickercalendar.year_selected)
 	#Format time
+	if hours.get() < 10:
+	    hour = "0"+str(hours.get())
+	    print(date +" "+hour+":00")
+	else:
+	    hour = str(hours.get())
+	    print(date +" "+str(hours.get())+":00")
 	
-	
-	time = str(hours.get())+":00"
-	presenceList = ["aloha"]
-	#with open ("appointments.txt",'w',newline="") as appFile:
-	#	writer = csv.writer(appFile)
-	#	writeList = [name.get(),date,time,preseceList]
-	#	writer.writerow(writeList)
-	#	appFile.close()
    
 	with open("dates.txt") as f:
 		for line in f:
-			if line.startswith(date +" "+str(hours.get())+":00" ):
-				print(line.split(" ")[2])
-				present = line.split(" ")[2]
+			if line.startswith(date +" "+hour+":00"):
+				print(line.split("'")[1:])
+				present = line.split("'")[1:]
+				#print(re.findall("['(.*?)']", str(present)))
 				print(present)
-				subprocess.call(["python3","showSheet.py"] + present)
+		subprocess.call(["python3","showSheet.py"] + present)
+		print(date,hour,present)
 					
 					
-				#if word == date:   # create a list of lists
-		#for i, x in enumerate(lis):   #print the list items 
-		#	print("{}".format(x))
+			
 	
 	
-	print(date,time,present)
-	#messagebox.showinfo("Success!","Appointment made!")
-	#calendarViewFrame = tk.Frame(userFrame, borderwidth=5, bg="black")
-	#calendarViewFrame.grid(row=2, column=1, columnspan=5)
-	#viewCalendar = CalendarView(calendarViewFrame, {name.get()})
+
 	
 	
 
@@ -86,22 +93,7 @@ bookAppointment = tk.Frame(root)
 
 bookAppointment.grid(row=0,column=0, sticky='news')
 bookAppointment.configure(bg='black')
-	
-with open("users.txt",'r') as userFile:
-		reader = csv.reader(userFile)
-		for row in reader:
-			#removes empty list from loop
-			if len(row)>0:
-				print(row[0]+" has logged in!")
-				#Set welcome message
-				loggedInLabel.set("Welcome, "+row[0])
-				# Calendar View
-				#global calendarViewFrame
-				#calendarViewFrame = tk.Frame(userFrame, borderwidth=5, bg="black")
-				#calendarViewFrame.grid(row=2, column=1, columnspan=5)
-				#viewCalendar = CalendarView(calendarViewFrame, {row[0]})
-				name.set(row[0])
-				#raiseFrame(userFrame)
+
 
 
 
